16779293aa9b09bc16c75023e8a51fab5ec3d363
karlmuhust <karlmuhust@gmail.com>
2025-02-10 22:53:05 +0200

    git

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: false,
-  distDir: ".next",
   output: "export",
   images: {
     unoptimized: true,
